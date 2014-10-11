07724f1454b905a0192fb1f3e8200862807d7b49
DarkholmeTenk <dark@darkhol.me>
2014-10-11 16:51:51 +0100

    Add to chat mode of screwdriver
    Fixes DarkholmeTenk/TardisModIssues#9

--- src/src/main/java/tardis/common/items/TardisSonicScrewdriverItem.java
+++ src/src/main/java/tardis/common/items/TardisSonicScrewdriverItem.java
@@ -1,12 +1,14 @@
 package tardis.common.items;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import tardis.api.TardisScrewdriverMode;
-
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 
 public class TardisSonicScrewdriverItem extends TardisAbstractItem
@@ -53,7 +55,7 @@ public class TardisSonicScrewdriverItem extends TardisAbstractItem
 		if(is != null)
 		{
 			TardisScrewdriverMode mode = getMode(is);
-			infoList.add("Screwdriver mode: " + mode.toString());
+			infoList.add("Mode: " + mode.toString());
 			if(mode.equals(TardisScrewdriverMode.Schematic))
 			{
 				String schemaName = getSchema(is);
@@ -68,13 +70,24 @@ public class TardisSonicScrewdriverItem extends TardisAbstractItem
 	@Override
 	public ItemStack onItemRightClick(ItemStack is, World world, EntityPlayer player)
     {
-		if(player.isSneaking())
+		if(!world.isRemote && player.isSneaking())
 		{
-			
 			TardisScrewdriverMode mode = getMode(is);
 			int newValue = (mode.ordinal() + 1) % TardisScrewdriverMode.values().length;
 			is.stackTagCompound.setInteger("screwdriverMode", newValue);
 			getMode(is);
+			ArrayList<Object> list = new ArrayList<Object>();
+			addInformation(is,player,list,false);
+			for(Object o: list)
+			{
+				if(o instanceof String)
+				{
+					ChatMessageComponent c = new ChatMessageComponent();
+					c.setColor(EnumChatFormatting.AQUA);
+					c.addText("[Sonic Screwdriver]" + (String)o);
+					player.sendChatToPlayer(c);
+				}
+			}
 		}
         return is;
     }
