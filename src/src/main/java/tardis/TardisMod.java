059ee22c04765aa2c6534dc2b6977089110b7a3a
DarkholmeTenk <dark@darkhol.me>
2014-10-18 02:58:42 +0100

    Stuff
    Updated version to 0.09
    Added new corridor/corridor4Way schema

--- src/src/main/java/tardis/TardisMod.java
+++ src/src/main/java/tardis/TardisMod.java
@@ -51,7 +51,7 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid="TardisMod",name="Tardis Mod",version="0.08",dependencies="required-after:FML")
+@Mod(modid="TardisMod",name="Tardis Mod",version="0.09",dependencies="required-after:FML")
 @NetworkMod(channels = { "TardisModChannel","TardisTrans","TardisDR" }, clientSideRequired = true, serverSideRequired = true, packetHandler = TardisPacketHandler.class, connectionHandler=TardisConnectionHandler.class)
 public class TardisMod
 {
