938a680bf2b3d1cce6a53925f2035f26a8ed065e
DarkholmeTenk <dark@darkhol.me>
2014-12-09 15:50:07 +0000

    Initial work on the Lab. Got some TE .obj renderers working.

---
+++ src/src/main/java/tardis/common/tileents/LabTileEntity.java
@@ -0,0 +1,18 @@
+package tardis.common.tileents;
+
+import net.minecraft.nbt.NBTTagCompound;
+
+public class LabTileEntity extends TardisAbstractTileEntity
+{
+
+	@Override
+	public void writeTransmittable(NBTTagCompound nbt)
+	{
+	}
+
+	@Override
+	public void readTransmittable(NBTTagCompound nbt)
+	{
+	}
+
+}
