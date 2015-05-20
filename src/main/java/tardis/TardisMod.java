6c05bd25330b5642d11d03730814d8deae1c6b21
DarkholmeTenk <shaney.booth@gmail.com>
2015-05-20 22:09:26 +0100

    Version bump

--- src/main/java/tardis/TardisMod.java
+++ src/main/java/tardis/TardisMod.java
@@ -78,7 +78,7 @@ import cpw.mods.fml.common.event.FMLServerAboutToStartEvent;
 import cpw.mods.fml.common.event.FMLServerStartedEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 
-@Mod(modid = "TardisMod", name = "Tardis Mod", version = "0.9825", dependencies = "required-after:FML; required-after:darkcore@[0.22,]; after:CoFHCore; after:appliedenergistics2; after:Waila")
+@Mod(modid = "TardisMod", name = "Tardis Mod", version = "0.985", dependencies = "required-after:FML; required-after:darkcore@[0.25,]; after:CoFHCore; after:appliedenergistics2; after:Waila")
 public class TardisMod implements IConfigHandlerMod
 {
 	@Instance
