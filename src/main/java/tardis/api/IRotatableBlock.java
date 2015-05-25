83e704110848dc377c8f8b1a3762d29f8ca11b74
DarkholmeTenk <shaney.booth@gmail.com>
2015-05-25 15:55:40 +0100

    Implemented rotatable api

---
+++ src/main/java/tardis/api/IRotatableBlock.java
@@ -0,0 +1,6 @@
+package tardis.api;
+
+public interface IRotatableBlock
+{
+	public int rotatedMeta(int oldMeta, int oldFacing, int newFacing);
+}
--- src/main/java/tardis/common/core/schema/SchemaRotationHandler.java
+++ src/main/java/tardis/common/core/schema/SchemaRotationHandler.java
@@ -3,27 +3,28 @@ package tardis.common.core.schema;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.init.Blocks;
+import tardis.api.IRotatableBlock;
 import tardis.common.core.TardisOutput;
 
 public class SchemaRotationHandler
 {
-	
+
 	private static boolean facingOpposite(int a, int b)
 	{
-		if((a == 0 && b == 2) || (b == 0 && a == 2))
+		if(((a == 0) && (b == 2)) || ((b == 0) && (a == 2)))
 			return true;
-		if((a == 1 && b == 3) || (b == 1 && a == 3))
+		if(((a == 1) && (b == 3)) || ((b == 1) && (a == 3)))
 			return true;
 		return false;
 	}
-	
+
 	private static int getChestMetadata(int blockMeta, int oldFacing, int newFacing)
 	{
 		int newMeta = getBaseMetadata(blockMeta-2,oldFacing,newFacing)+2;
 		TardisOutput.print("TSRH", "ChestRot:"+blockMeta+","+newMeta+" for facing:"+oldFacing+","+newFacing);
 		return newMeta;
 	}
-	
+
 	private static int getStairsMetadata(int blockMeta,int oldFacing, int newFacing)
 	{
 		int add   = (blockMeta / 4) * 4;
@@ -32,7 +33,7 @@ public class SchemaRotationHandler
 		TardisOutput.print("TSRH", "Stair:"+blockMeta+","+add+"->"+newMeta+"#"+oldFacing+","+newFacing);
 		return newMeta+add;
 	}
-	
+
 	private static int getBaseMetadata(int blockMeta,int oldFacing, int newFacing)
 	{
 		if(facingOpposite(oldFacing,newFacing))
@@ -45,7 +46,7 @@ public class SchemaRotationHandler
 			case 3: return 2;
 			}
 		}
-		else if((oldFacing + 1)%4 == newFacing)
+		else if(((oldFacing + 1)%4) == newFacing)
 		{
 			switch(blockMeta)
 			{
@@ -55,7 +56,7 @@ public class SchemaRotationHandler
 			case 3: return 1;
 			}
 		}
-		else if((newFacing+1)%4 == oldFacing)
+		else if(((newFacing+1)%4) == oldFacing)
 		{
 			switch(blockMeta)
 			{
@@ -67,7 +68,7 @@ public class SchemaRotationHandler
 		}
 		return blockMeta;
 	}
-	
+
 	public static int getNewMetadata(Block block, int blockMeta, int oldFacing, int newFacing)
 	{
 		if(oldFacing == newFacing)
@@ -75,7 +76,9 @@ public class SchemaRotationHandler
 		if(block == Blocks.chest)
 			return getChestMetadata(blockMeta,oldFacing,newFacing);
 		if(block instanceof BlockStairs)
-				return getStairsMetadata(blockMeta, oldFacing, newFacing);
+			return getStairsMetadata(blockMeta, oldFacing, newFacing);
+		if(block instanceof IRotatableBlock)
+			return ((IRotatableBlock)block).rotatedMeta(blockMeta, oldFacing, newFacing);
 		return blockMeta;
 	}
 }
