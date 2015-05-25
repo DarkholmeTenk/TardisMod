432b7159812d31694e17816baa451e52029a628d
DarkholmeTenk <shaney.booth@gmail.com>
2015-05-25 16:18:16 +0100

    Whoops

--- src/main/java/tardis/common/tileents/CoreTileEntity.java
+++ src/main/java/tardis/common/tileents/CoreTileEntity.java
@@ -28,6 +28,7 @@ import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.World;
