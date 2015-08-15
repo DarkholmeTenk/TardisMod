19ff8c4d38fe715a9f8fbc3d586576ac71748ece
DarkholmeTenk <shaney.booth@gmail.com>
2015-08-15 16:09:45 +0100

    Preparations for upgrade crafting

--- src/main/java/tardis/common/tileents/extensions/CraftingComponentType.java
+++ src/main/java/tardis/common/tileents/extensions/CraftingComponentType.java
@@ -7,14 +7,15 @@ import tardis.common.core.TardisOutput;
 public enum CraftingComponentType {
 	CHRONOSTEEL("IngotChronosteel"),
 	DALEKANIUM("IngotDalek"),
-	KONTRON("CrystalKontron");
-	
+	KONTRON("CrystalKontron"),
+	UPGRADE("Upgrade");
+
 	public final String name;
 	CraftingComponentType(String passedName)
 	{
 		name=passedName;
 	}
-	
+
 	public ItemStack getIS(int amount)
 	{
 		TardisOutput.print("CCT", "Getting IS for :" + name + " returning " + ordinal(),TardisOutput.Priority.OLDDEBUG);
