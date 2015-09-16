46a6843a4d9c35f30d7077a4911b97f6596ac4cb
DarkholmeTenk <shaney.booth@gmail.com>
2015-09-16 12:27:42 +0100

    Unlinked TARDIS exteriors should not chunkload

--- src/main/java/tardis/common/tileents/TardisTileEntity.java
+++ src/main/java/tardis/common/tileents/TardisTileEntity.java
@@ -298,7 +298,7 @@ public class TardisTileEntity extends AbstractTileEntity implements IChunkLoader
 	@Override
 	public boolean shouldChunkload()
 	{
-		return true;
+		return linkedDimension != null;
 	}
 
 	@Override
