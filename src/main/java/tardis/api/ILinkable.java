fed27cf65532de327ba2a324a287b9ee63da5b09
DarkholmeTenk <shaney.booth@gmail.com>
2015-09-19 15:08:40 +0100

    Cleanup of ILinkable to allow it to work for blocks

--- src/main/java/tardis/api/ILinkable.java
+++ src/main/java/tardis/api/ILinkable.java
@@ -6,10 +6,16 @@ import java.util.Set;
 
 import net.minecraft.entity.player.EntityPlayer;
 
+/**
+ * SimpleCoordStore link is the position of the block which is being linked to other. For TileEntities, this is effectively this.
+ * @author dark
+ *
+ */
 public interface ILinkable
 {
-	public boolean link(EntityPlayer pl, SimpleCoordStore other);
-	public boolean unlink(EntityPlayer pl, SimpleCoordStore other);
-	public boolean unlink(EntityPlayer pl);
-	public Set<SimpleCoordStore> getLinked();
+	public boolean link(EntityPlayer pl, SimpleCoordStore link, SimpleCoordStore other);
+	public boolean unlink(EntityPlayer pl, SimpleCoordStore link, SimpleCoordStore other);
+	public boolean unlink(EntityPlayer pl, SimpleCoordStore link);
+	public Set<SimpleCoordStore> getLinked(SimpleCoordStore link);
+	public boolean isLinkable();
 }
