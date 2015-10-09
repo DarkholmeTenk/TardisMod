48d3e3c47af915e53b4ab23a7f4da77317578e41
DarkholmeTenk <shaney.booth@gmail.com>
2015-10-09 09:43:42 +0100

    Prep

---
+++ src/main/java/tardis/Configs.java
@@ -0,0 +1,16 @@
+package tardis;
+
+public class Configs
+{
+	/*
+	 * LEVEL CONFIG
+	 */
+	public static int											xpBase				= 80;
+	public static int											xpInc				= 20;
+	public static int											rfBase				= 50000;
+	public static int											rfInc				= 50000;
+	public static int											rfPerT				= 4098;
+	public static int											maxFlu				= 32000;
+	public static int											numTanks			= 5;
+	public static int											numInvs				= 30;
+}
